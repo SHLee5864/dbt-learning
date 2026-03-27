7ec24ea299900e04119fddd31d098c674002d5cb
SHLee5864 <lsh5864@gmail.com>
2026-03-27 14:12:19 +0100

    add new orders seed for incremental tests

---
+++ jaffle_shop/models/marts/fct_orders_incremental_append.sql
@@ -0,0 +1,33 @@
+-- This is an incremental model that builds on top of the fct_orders model.
+--It will only process new records based on the order_date column.
+
+{{ config(
+    materialized='incremental',
+     incremental_strategy='append'
+) }}
+
+with base as (
+    select
+        o.order_id,
+        o.order_date,
+        o.customer_id,
+        c.first_name,
+        c.last_name,
+        c.email,
+        o.product_id,
+        p.product_name,
+        p.category,
+        o.amount,
+        p.price,
+        o.amount * p.price as total_amount
+    from {{ ref('stg_orders') }} o
+    left join {{ ref('stg_customers') }} c on o.customer_id = c.customer_id
+    left join {{ ref('stg_products') }} p on o.product_id = p.product_id
+)
+
+select *
+from base
+
+{% if is_incremental() %}
+where order_date > (select max(order_date) from {{ this }})
+{% endif %}
